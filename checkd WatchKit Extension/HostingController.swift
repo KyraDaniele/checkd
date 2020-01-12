0d0cd95e138b312eb79f8c047855ed98e11ea8d0
Kyra Mellema <kemellema@gmail.com>
2020-01-11 21:55:21 -0500

    Initial Commit

---
+++ checkd WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  checkd WatchKit Extension
+//
+//  Created by Kyra Mellema on 1/11/20.
+//  Copyright © 2020 Kyra Mellema. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
